2629ae66f7cbff4abbb600a6ca2ad5ef1382d002
Evandro Barbosa dos Reis <evandrobarbosadosreis@gmail.com>
2020-12-13 09:45:44 -0300

    Add modelo de usuário + DbContext com InMemoryDatabase

---
+++ webapi/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using webapi.Models;
+
+namespace webapi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        { }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+    }
+}
--- webapi/Startup.cs
+++ webapi/Startup.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using webapi.Data;
 
 namespace webapi
 {
@@ -25,8 +20,8 @@ namespace webapi
 
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+            services.AddDbContext<DataContext>(o => o.UseInMemoryDatabase("database"));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "webapi", Version = "v1" });
